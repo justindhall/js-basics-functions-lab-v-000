13bd18dc560fa133bf1fe59e77a49f17c29e3357
Justin Hall <hall.justin.d@gmail.com>
2018-05-17 15:53:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(pickupLocation){
+  
+  return Math.abs(42-pickupLocation);
+  
+}
+
+function distanceFromHqInFeet(pickupLocation){
+  
+  return distanceFromHqInBlocks(pickupLocation) * 264;
+  
+}
+
+function distanceTravelledInFeet(pickupLocation, dropoffLocation){
+  
+  return Math.abs(pickupLocation - dropoffLocation) * 264;
+  
+}
+
+function validFare(distance){
+  
+  const valid;
+  
+  if(distance > 2500){
+    
+  }
+  
+}
+
+function calculatesFarePrice(pickupLocation, dropoffLocation){
+  
+  
+  
+}
